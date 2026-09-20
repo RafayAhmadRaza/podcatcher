db892c583431356220a89cdd8248bc7b3345b72b
Rafay Ahmad Raza <rafayahmadraza@gmail.com>
2026-09-20 13:42:53 +0500

    basic download features added

--- src/podcatcher/downloader.py
+++ src/podcatcher/downloader.py
@@ -3,15 +3,15 @@ from pathlib import Path
 path = Path("Podcaster/downloader")
 
 
-def download_ep(url):
-    file_path = path / "episode.mp3"
+def download_ep(title,episode_name,url):
+    file_path = path / title /f"{episode_name}.mp3"
 
     if file_path.exists():
         print("Already downloaded")
         return
 
     path.mkdir(parents=True, exist_ok=True)
-
+    file_path.parent.mkdir(parents=True,exist_ok=True)
     file = httpx.get(url, follow_redirects=True)
     file.raise_for_status()
 
@@ -20,4 +20,4 @@ def download_ep(url):
 
 
 if __name__ == "__main__":
-    download_ep("https://dts.podtrac.com/redirect.mp3/mgln.ai/e/211/rss.art19.com/episodes/761a2733-c9c2-4fec-8c15-627cd3472417.mp3")
+    download_ep("LINUX Unplugged","Episode 1: Too Much Choice","https://dts.podtrac.com/redirect.mp3/mgln.ai/e/211/rss.art19.com/episodes/761a2733-c9c2-4fec-8c15-627cd3472417.mp3")
