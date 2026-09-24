916f1a27a509ea7faa8e49dba1ec8996e193d7df
Rafay Ahmad Raza <rafayahmadraza@gmail.com>
2026-09-24 09:58:42 +0500

    Basic TUI Display Made

--- src/podcatcher/tui.py
+++ src/podcatcher/tui.py
@@ -1,4 +1,5 @@
 from textual.app import App, ComposeResult
+from textual import events
 from textual.containers import Container, Vertical, Horizontal
 from textual.widgets import Static, Header, Footer,ListItem,ListView
 from .database import get_podcasts
@@ -9,17 +10,38 @@ SELECTED_PDC = 0
 
 class PodcatcherApp(App):
     CSS_PATH = "tui.tcss"
-    def on_list_view_selected(self,event: ListView.Selected):
-        global SELECTED_PDC
-        message = self.query_one("#message",Static)
-        message.update(f"Selected: {event.index}")
-        SELECTED_PDC = event.index
-        episodes_list = self.query_one("#episodes-list",ListView)
-        episodes_list.clear()
-        pdc = podcasts[SELECTED_PDC]
+    def on_key(self,event: events.Key) -> None:
+        if event.key == "left":
+            message = self.query_one("#message",Static)
+            message.update(f"Selected: j")
+            podcasts_list = self.query_one("#podcast-list")
+            podcasts_list.focus()
+        if event.key == "right":
+            
+            message = self.query_one("#message",Static)
+            message.update(f"Selected: l")
+            ep_list = self.query_one("#episodes-list")
+            ep_list.focus()
 
-        for ep in pdc.episodes:
-            episodes_list.append(ListItem(Static(ep.title)))
+    def on_list_view_selected(self,event: ListView.Selected):
+      
+        podcast_list = self.query_one("#podcast-list",ListView)
+        global SELECTED_PDC
+
+        if event.list_view == podcast_list:
+
+            episodes_list = self.query_one("#episodes-list",ListView)
+        
+            
+            SELECTED_PDC = event.index
+            
+            
+            episodes_list.clear()
+            
+            pdc = podcasts[SELECTED_PDC]
+
+            for ep in pdc.episodes:
+                episodes_list.append(ListItem(Static(ep.title)))
 
     def compose(self) -> ComposeResult:
         global SELECTED_PDC
